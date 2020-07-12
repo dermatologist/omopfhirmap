3aa7f82859bd4d877b87c0fd1547369e912dfd89
Bellraj Eapen <github@gulfdoctor.net>
2020-07-12 04:55:41 -0700

    working commandline

--- src/main/java/com/canehealth/omopfhirmap/OmopfhirmapApplication.java
+++ src/main/java/com/canehealth/omopfhirmap/OmopfhirmapApplication.java
@@ -2,12 +2,29 @@ package com.canehealth.omopfhirmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
-public class OmopfhirmapApplication {
-
+public class OmopfhirmapApplication  implements CommandLineRunner {
+ 
+    private static Logger LOG = LoggerFactory
+      .getLogger(OmopfhirmapApplication.class);
 	public static void main(String[] args) {
+		LOG.info("STARTING THE APPLICATION");
+
 		SpringApplication.run(OmopfhirmapApplication.class, args);
+		LOG.info("APPLICATION FINISHED");
 	}
+
+	@Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+ 
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+    }
 
 }
