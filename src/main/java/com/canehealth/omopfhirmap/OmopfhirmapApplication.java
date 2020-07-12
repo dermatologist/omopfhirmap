7df0657da971bfa32cc73420351eddd486bc16be
Bellraj Eapen <github@gulfdoctor.net>
2020-07-12 03:34:38 -0700

    first commit

---
+++ src/main/java/com/canehealth/omopfhirmap/OmopfhirmapApplication.java
@@ -0,0 +1,13 @@
+package com.canehealth.omopfhirmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmopfhirmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmopfhirmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canehealth/omopfhirmap/OmopfhirmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canehealth.omopfhirmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmopfhirmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
