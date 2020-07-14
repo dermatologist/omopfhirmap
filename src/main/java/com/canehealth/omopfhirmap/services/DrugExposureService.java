972f60254e4d95aef74bc96775705c0ddddc4ec4
Bellraj Eapen <github@gulfdoctor.net>
2020-07-14 11:44:13 -0700

    feat: Drug Exposure

---
+++ src/main/java/com/canehealth/omopfhirmap/services/DrugExposureService.java
@@ -0,0 +1,30 @@
+package com.canehealth.omopfhirmap.services;
+
+import com.canehealth.omopfhirmap.repositories.DrugExposureRepository;
+import com.canehealth.omopfhirmap.models.DrugExposure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.List;
+
+@Service
+public class DrugExposureService {
+
+    @Autowired
+    private DrugExposureRepository drugExposureRepository;
+
+    public List<DrugExposure> list(){
+        return drugExposureRepository.findAll();
+    }
+
+    public List<DrugExposure> listByPerson(Integer personId){
+        return drugExposureRepository.findByPersonId(personId);
+    }
+
+    public List<DrugExposure> listByPersonAndPeriod(Integer personId, Date start, Date end){
+        return drugExposureRepository.findByPersonIdAndDrugExposureStartDateBetween(personId, start, end);
+    }
+    
+}
