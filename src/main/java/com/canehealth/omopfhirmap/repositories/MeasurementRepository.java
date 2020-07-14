b1f10891d5e5498be81e6749856d2fdc31a2d6ae
Bellraj Eapen <github@gulfdoctor.net>
2020-07-14 11:32:15 -0700

    feat: Measurement by person and period

--- src/main/java/com/canehealth/omopfhirmap/repositories/MeasurementRepository.java
+++ src/main/java/com/canehealth/omopfhirmap/repositories/MeasurementRepository.java
@@ -5,11 +5,14 @@ import com.canehealth.omopfhirmap.models.Measurement;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer>{
 
     List<Measurement> findByPersonId(Integer personId);
+    List<Measurement> findByPersonIdAndMeasurementDateBetween(Integer personId, Date start, Date end);
+
     
 }
