8608f6fb0f96ba706b038bb0e1335988611ff7a1
Bellraj Eapen <github@gulfdoctor.net>
2020-07-15 13:24:02 -0700

    feat: BaseMapper fetch

--- src/main/java/com/canehealth/omopfhirmap/mapping/BaseMapper.java
+++ src/main/java/com/canehealth/omopfhirmap/mapping/BaseMapper.java
@@ -1,13 +1,31 @@
 package com.canehealth.omopfhirmap.mapping;
 
+import java.util.List;
+
 import com.canehealth.omopfhirmap.fhir.R4Bundle;
 import com.canehealth.omopfhirmap.models.Cohort;
+import com.canehealth.omopfhirmap.services.CohortService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.Data;
 
 @Data
 public class BaseMapper {
+
+    @Autowired
+    CohortService cohortService;
     
+    private List<Cohort> cohorts;
     private Cohort cohort;
     private R4Bundle r4Bundle;
+    private int cohortId = 0;
+    private int cohortSize = 0;
+
+    public void fetchCohort(){
+        if(cohortId>0){
+            this.cohorts = cohortService.listByCohort(this.cohortId);
+            this.cohortSize = cohorts.size();
+        }
+    }
 }
