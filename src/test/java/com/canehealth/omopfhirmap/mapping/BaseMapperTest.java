aa7946aadb01c6fa296e29349964bd72b2fe526e
Bell Eapen <github_public@gulfdoctor.net>
2020-07-15 17:04:08 -0400

    Add BaseMapperTest

---
+++ src/test/java/com/canehealth/omopfhirmap/mapping/BaseMapperTest.java
@@ -0,0 +1,30 @@
+package com.canehealth.omopfhirmap.mapping;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseMapperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void fetchCohort() {
+    }
+
+    @Test
+    void fetchOmopResources() {
+    }
+
+    @Test
+    void createBundle() {
+    }
+}
