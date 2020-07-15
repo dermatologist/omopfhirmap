e2626bc971b83c18de59f5f8e24922fa5b397050
Bellraj Eapen <github@gulfdoctor.net>
2020-07-14 20:50:11 -0700

    feat: BaseFetcher

---
+++ src/main/java/com/canehealth/omopfhirmap/mapping/BaseFetcher.java
@@ -0,0 +1,35 @@
+package com.canehealth.omopfhirmap.mapping;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import com.canehealth.omopfhirmap.services.BaseService;
+import com.canehealth.omopfhirmap.models.Cohort;
+
+import lombok.Data;
+
+@Data
+public abstract class BaseFetcher<S extends BaseService<?, M>, M> {
+
+    private List<Cohort> cohorts;
+    private List<M> omopResources;
+
+    private final Class<S> OmopService;
+
+    // @IMPORTANT: This constructor is required for generics
+    public BaseFetcher(Class<S> OmopService) {
+        this.OmopService = OmopService;
+    }
+
+    public void fetch() throws InstantiationException, IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException {
+        for(Cohort cohort: cohorts){
+            // @IMPORTANT: See how the instance is created
+            List<M> omopTemp = this.OmopService.getDeclaredConstructor().newInstance().listByPersonAndPeriod(
+                cohort.getSubjectId(), cohort.getCohortStartDate(), cohort.getCohortEndDate());
+
+            omopResources.addAll(omopTemp);
+        }
+    }
+    
+}
--- src/main/java/com/canehealth/omopfhirmap/services/BaseService.java
+++ src/main/java/com/canehealth/omopfhirmap/services/BaseService.java
@@ -5,13 +5,13 @@ import java.sql.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class BaseService <j extends JpaRepository<?, Integer>, c>{
+public abstract class BaseService <J extends JpaRepository<?, Integer>, C>{
     
     
-    public abstract List<c> list();
+    public abstract List<C> list();
 
-    public abstract List<c> listByPerson(Integer personId);
+    public abstract List<C> listByPerson(Integer personId);
 
-    public abstract List<c> listByPersonAndPeriod(Integer personId, Date start, Date end);
+    public abstract List<C> listByPersonAndPeriod(Integer personId, Date start, Date end);
     
 }
