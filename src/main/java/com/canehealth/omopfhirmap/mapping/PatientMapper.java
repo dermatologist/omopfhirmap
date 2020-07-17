36c0b33c26248db52b926f9ddb1d901524a46569
Bell Eapen <github_public@gulfdoctor.net>
2020-07-17 12:11:23 -0400

    feat: BaseMapper stage 2

--- src/main/java/com/canehealth/omopfhirmap/mapping/PatientMapper.java
+++ src/main/java/com/canehealth/omopfhirmap/mapping/PatientMapper.java
@@ -2,6 +2,7 @@ package com.canehealth.omopfhirmap.mapping;
 
 import com.canehealth.omopfhirmap.models.*;
 import com.canehealth.omopfhirmap.fhir.*;
+import com.canehealth.omopfhirmap.utils.AddOmopKeyAsIdentifier;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hl7.fhir.r4.model.Identifier;
@@ -19,10 +20,13 @@ public class PatientMapper extends BaseMapper<Person, R4Patient>{
     private String myIdentifierSystem;
 
     public void mapOmopToFhir(){
-        Identifier identifier = new Identifier();
-        identifier.setSystem(myIdentifierSystem);
-        identifier.setValue(this.omopResource.getPersonId().toString());
-        this.fhirResource.addIdentifier(identifier);
+//        Identifier identifier = new Identifier();
+//        identifier.setSystem(myIdentifierSystem);
+//        identifier.setValue(this.omopResource.getPersonId().toString());
+//        this.fhirResource.addIdentifier(identifier);
+
+        AddOmopKeyAsIdentifier<Person, R4Patient> addOmopKeyAsIdentifier = new AddOmopKeyAsIdentifier<Person, R4Patient>();
+        this.fhirResource = addOmopKeyAsIdentifier.add(this.omopResource, this.fhirResource);
 
     }
 }
