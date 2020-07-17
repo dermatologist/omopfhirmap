8b26c3ad9ab90e7212a8b45667afb3a789aa0011
Bell Eapen <github_public@gulfdoctor.net>
2020-07-17 12:44:49 -0400

    feat: complete add identifier

--- src/main/java/com/canehealth/omopfhirmap/mapping/PatientMapper.java
+++ src/main/java/com/canehealth/omopfhirmap/mapping/PatientMapper.java
@@ -1,15 +1,12 @@
 package com.canehealth.omopfhirmap.mapping;
 
-import com.canehealth.omopfhirmap.models.*;
-import com.canehealth.omopfhirmap.fhir.*;
+import com.canehealth.omopfhirmap.fhir.R4Patient;
+import com.canehealth.omopfhirmap.models.Person;
 import com.canehealth.omopfhirmap.utils.AddOmopKeyAsIdentifier;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hl7.fhir.r4.model.Identifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -20,13 +17,8 @@ public class PatientMapper extends BaseMapper<Person, R4Patient>{
     private String myIdentifierSystem;
 
     public void mapOmopToFhir(){
-//        Identifier identifier = new Identifier();
-//        identifier.setSystem(myIdentifierSystem);
-//        identifier.setValue(this.omopResource.getPersonId().toString());
-//        this.fhirResource.addIdentifier(identifier);
-
-        AddOmopKeyAsIdentifier<Person, R4Patient> addOmopKeyAsIdentifier = new AddOmopKeyAsIdentifier<Person, R4Patient>();
-        this.fhirResource = addOmopKeyAsIdentifier.add(this.omopResource, this.fhirResource);
+        AddOmopKeyAsIdentifier<Person> addOmopKeyAsIdentifier = new AddOmopKeyAsIdentifier<>();
+        this.fhirResource.addIdentifier(addOmopKeyAsIdentifier.add(this.omopResource, myIdentifierSystem));
 
     }
 }
--- src/main/java/com/canehealth/omopfhirmap/utils/AddOmopKeyAsIdentifier.java
+++ src/main/java/com/canehealth/omopfhirmap/utils/AddOmopKeyAsIdentifier.java
@@ -1,35 +1,15 @@
 package com.canehealth.omopfhirmap.utils;
 
-import com.canehealth.omopfhirmap.fhir.R4Patient;
 import com.canehealth.omopfhirmap.models.BaseModel;
-import lombok.Data;
 import org.hl7.fhir.r4.model.Identifier;
-import org.hl7.fhir.r4.model.Resource;
-import org.hl7.fhir.r4.model.ResourceType;
-import org.springframework.beans.factory.annotation.Value;
 
-import javax.persistence.Entity;
+public class AddOmopKeyAsIdentifier<M extends BaseModel> {
 
-@Data
-public class AddOmopKeyAsIdentifier<M extends BaseModel, F extends Resource> {
-
-    public M omopResource;
-    public F fhirResource;
-
-    @Value("${omopfhir.system.name}")
-    private String myIdentifierSystem;
-
-    public F add(M omopResource, F fhirResource){
+    public Identifier add(M omopResource, String myIdentifierSystem){
         Identifier identifier = new Identifier();
         identifier.setSystem(myIdentifierSystem);
         identifier.setValue(omopResource.getId().toString());
-        if(this.fhirResource.getResourceType() == ResourceType.Patient){
-            R4Patient patient = (R4Patient) this.fhirResource;
-            patient.addIdentifier(identifier);
-            this.fhirResource = (F) patient;
-            return (F) patient;
-        }
-        return (F) fhirResource;
+        return identifier;
     }
 
 
