3dbf016b9667463ee647ced1ba90fb7be512e1b4
Bellraj Eapen <github@gulfdoctor.net>
2020-07-29 15:17:17 -0700

    feat: CqlToElmTranslator. PheMA dependancy

--- src/test/java/com/canehealth/omopfhirmap/OmopfhirmapApplicationTests.java
+++ src/test/java/com/canehealth/omopfhirmap/OmopfhirmapApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.concurrent.ThreadLocalRandom;
 
+import edu.phema.elm_to_omop.api.CqlToElmTranslator;
+
 @SpringBootTest
 class OmopfhirmapApplicationTests {
 
@@ -21,6 +23,15 @@ class OmopfhirmapApplicationTests {
 		randomNum = 2;
 		String[] args = {"tofhirbundle", String.valueOf(randomNum), "test-bundle.json"};
 		OmopfhirmapApplication.main(args);
+	}
+
+	@Test
+	void phemaTest(){
+		CqlToElmTranslator translator = new CqlToElmTranslator();
+
+        String cql = "define test: 1 + 1";
+
+        System.out.println(translator.cqlToElmJson(cql));
 	}
 
 }
